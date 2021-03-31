128d7b6345772d9dd6ee963578e8e6a8562d0cc5
Martins <67004169+chukwuekeuzoma@users.noreply.github.com>
2021-03-31 22:49:39 +0100

    update

--- src/Components/ProfilePage/Profilepage.jsx
+++ src/Components/ProfilePage/Profilepage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Fade from 'react-reveal/Fade'
 import "./ProfilePage.scss"
+import {Button } from '@material-ui/core';
+import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
 
 export default function Profilepage() {
     return (
@@ -9,12 +11,35 @@ export default function Profilepage() {
                 <Fade big  duration={1000}>
                     <div className="Profile_container">
                         <h2>Profile</h2>
-                        <h2>
-                            input will be place here
-                        </h2>
                     </div>
                     <div className="PR_Grid_container">
-                        <div className="PR_content_one">1</div>
+                        <div className="PR_content_one">
+                            <div className="PR_Header">
+                                <div className="PR_Header_content">
+                                    <span className="PR_Numbers">Numbers of <br/> request</span>
+                                    <span className="PR_Numbers_one">2</span>
+                                </div>
+                                <div  className="PR_Header_content_two">
+                                    <span className="PR_Total">Total amount <br/> Requested</span>
+                                    <span className="PR_Total_one">N&nbsp;23,000</span>
+                                </div>
+                                <div className="PR_Number_Days_container">
+                                    <div className="PR_Days_container">
+                                        <div className="PR_Number_of_days"><span>Number of<br/> of Days worked</span></div>
+                                        <CalendarTodayIcon className ="PR_Icon"/><br/> 
+                                    </div>
+                                    <div className="PR_Days_container_two">
+                                        <div className="PR_Figures"><span>15</span></div>
+                                        <div className="PR_Date"><span>Feb<br/>2021</span></div>  
+                                    </div>
+                                </div>
+                                <div  className="PR_Header_content_one">
+                                    <span className="PR_Avaluable">Avaluable</span>
+                                    <span className="PR_Avaluable_one">N36,250.00</span>
+                                    <Button variant="outlined" className="PR_Header_Button">REQUEST PAYOUT</Button>
+                                </div>
+                            </div>
+                        </div>
                         <div className="PR_content_two">2</div>               
                     </div>
                 </Fade>
--- src/Components/Requestpage/Requestpage.jsx
+++ src/Components/Requestpage/Requestpage.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import "./Requestpage.scss"
+import "../ProfilePage/ProfilePage.scss"
 import Fade from 'react-reveal/Fade'
+import {Button } from '@material-ui/core';
+import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
+
 
 export default function Requestpage() {
     return (
@@ -13,7 +17,33 @@ export default function Requestpage() {
                     </h2>
                 </div>
                 <div className="RQ_Grid_container">
-                    <div className="RQ_content_one">1</div>
+                    <div className="RQ_content_one">
+                        <div className="PR_Header">
+                            <div className="PR_Header_content">
+                                <span className="PR_Numbers">Numbers of <br/> request</span>
+                                <span className="PR_Numbers_one">2</span>
+                            </div>
+                            <div  className="PR_Header_content_two">
+                                <span className="PR_Total">Total amount <br/> Requested</span>
+                                <span className="PR_Total_one">N&nbsp;23,000</span>
+                            </div>
+                            <div className="PR_Number_Days_container">
+                                <div className="PR_Days_container">
+                                    <div className="PR_Number_of_days"><span>Number of<br/> of Days worked</span></div>
+                                    <CalendarTodayIcon className ="PR_Icon"/><br/> 
+                                </div>
+                                <div className="PR_Days_container_two">
+                                    <div className="PR_Figures"><span>15</span></div>
+                                    <div className="PR_Date"><span>Feb<br/>2021</span></div>  
+                                </div>
+                            </div>
+                            <div  className="PR_Header_content_one">
+                                <span className="PR_Avaluable">Avaluable</span>
+                                <span className="PR_Avaluable_one">N36,250.00</span>
+                                <Button variant="outlined" className="PR_Header_Button">REQUEST PAYOUT</Button>
+                            </div>
+                        </div>
+                    </div>
                     <div className="RQ_content_two">2</div>               
                 </div>
             </Fade>
