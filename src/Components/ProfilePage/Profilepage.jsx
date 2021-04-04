22cf4f8a5c8e506913071676acd24518d5140f9a
Martins <67004169+chukwuekeuzoma@users.noreply.github.com>
2021-04-04 08:02:16 +0100

    update

--- src/Components/ProfilePage/Profilepage.jsx
+++ src/Components/ProfilePage/Profilepage.jsx
@@ -6,14 +6,87 @@ import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
 import DraftsIcon from '@material-ui/icons/Drafts';
 import LocalPhoneIcon from '@material-ui/icons/LocalPhone';
 import Employee from "../../images/employee.jpg"
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import {makeStyles, withStyles} from '@material-ui/core/styles';
+import Select from '@material-ui/core/Select';
+
+
+
+
+const useStyles = makeStyles({
+    root: {
+            '& label.Mui-focused': {
+                color: 'rgb(17, 17, 66)',
+              },
+              '& .MuiInputBase-root': {
+                color: 'rgb(17, 17, 66)'
+               },
+               '&.MuiTextField-root label' :{
+                top: 8,
+                bottom: 8
+              },
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                  borderColor: 'rgb(17, 17, 66) ',
+                },
+                '&:hover': {
+                  borderColor: 'rgb(17, 17, 66)',
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: 'rgb(17, 17, 66)',
+                },
+               
+              },
+    },
+    formControl: {
+        minWidth: 120,
+      },
+
+      Table: {
+        minWidth:50,
+      },
+    
+  })
 
 export default function Profilepage() {
+    
+    const classes =  useStyles();
+
     return (
         <>
             <div>
                 <Fade big  duration={1000}>
                     <div className="Profile_container">
                         <h2>Profile</h2>
+
+                        <form className={classes.root}>
+                            <FormControl variant="outlined" className="PR_select_textfield" size="small">
+                                <InputLabel htmlFor="outlined-age-native-simple">
+                                    Organization
+                                </InputLabel>
+                                <Select
+                                native
+                                // value={state.age}
+                                // onChange={handleChange}
+                                label="Organization"
+                                inputProps={{
+                                    name: 'age',
+                                    id: 'outlined-age-native-simple',
+                                }}
+                                >
+
+                                    <option aria-label="None" value=""/>
+                            
+                                    
+                                    <option value="Employer">
+                                       Employer
+                                    </option>
+                            
+                                </Select>
+
+                            </FormControl>
+                        </form>
                     </div>
                     <div className="PR_Grid_container">
                         <div className="PR_content_one">
@@ -46,7 +119,10 @@ export default function Profilepage() {
                         <div className="PR_content_two">
                             <div className="profile_content_container">
                                 <div className="profile_content">
-                                    <img src={Employee} alt="slideimage" className="PR_profile_image" />
+                                    
+                                         <img src={Employee} alt="slideimage" className="PR_profile_image"/>
+                                    
+                                   
                                     <div><span className="Profile_name">John Doe</span><br/><span className="Profile_name_content">Lorem ipsum dolor sit amet<br/>consectetu</span></div>
                                 </div>
                                 <div className="Profile_draft"><DraftsIcon/><br/>Email<br/>info@rapidkredit.com</div>
