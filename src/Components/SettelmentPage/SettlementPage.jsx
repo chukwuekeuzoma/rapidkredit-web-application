54eb8cf1ea1c5eeda0debb42f5a1b32742ebbade
Martins <67004169+chukwuekeuzoma@users.noreply.github.com>
2021-07-08 11:41:10 +0100

    settlement update

--- src/Components/SettelmentPage/SettlementPage.jsx
+++ src/Components/SettelmentPage/SettlementPage.jsx
@@ -8,23 +8,23 @@ import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
 export default function SettlementPage() {
     const columns = [
         { field: 'id', headerName: 'ID', width: 70 },
-        { field: 'firstName', headerName: 'First name', width: 130 },
-        { field: 'lastName', headerName: 'Last name', width: 130 },
+        { field: 'firstName', headerName: 'First name', width: 100 },
+        { field: 'lastName', headerName: 'Last name', width: 100 },
         {
           field: 'age',
           headerName: 'Age',
           type: 'number',
           width: 90,
         },
-        {
-          field: 'fullName',
-          headerName: 'Full name',
-          description: 'This column has a value getter and is not sortable.',
-          sortable: false,
-          width: 160,
-          valueGetter: (params) =>
-            `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
-        },
+        // {
+        //   field: 'fullName',
+        //   headerName: 'Full name',
+        //   description: 'This column has a value getter and is not sortable.',
+        //   sortable: false,
+        //   width: 160,
+        //   valueGetter: (params) =>
+        //     `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
+        // },
       ];
       
       const rows = [
@@ -38,6 +38,12 @@ export default function SettlementPage() {
         { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
         { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
       ];
+
+
+      const d = new Date()
+      const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "July", "Aug", "Sept", "Oct", "Nov", "Dec"]
+      const month = months[d.getMonth()]
+      const year = d.getFullYear()
 
     return (
         <>
@@ -67,7 +73,7 @@ export default function SettlementPage() {
                                 </div>
                                 <div className="ST_Icon_Container">
                                      <CalendarTodayIcon className ="ST_Icon"/><br/> 
-                                     <div className="ST_Date"><span>Feb<br/>2021</span></div>  
+                                     <div className="ST_Date"><span>{month}<br/>{year}</span></div>  
                                 </div>
                             </div>
 
@@ -85,16 +91,36 @@ export default function SettlementPage() {
                 <Fade big  duration={1000}>
                     <div className="ST_Grid_container_mobile">
                         <div className="ST_content_one_mobile">
-
+                            <div className="ST_Number_Days_container_mobile">
+                                <div className="ST_Days_container_mobile">
+                                    <div className="ST_Number_of_days_mobile"><span>Pending<br/>Settlement</span></div>
+                                </div>
+                                <div className="ST_Days_container_two_mobile">
+                                    <div className="ST_Figures_mobile"><span>0</span></div>
+                                </div>
+                            </div>
+                            <div className="ST_Icon_Container_mobile">
+                                     <CalendarTodayIcon className ="ST_Icon_mobile"/><br/> 
+                                     <div className="ST_Date_mobile"><span>{month}<br/>{year}</span></div>  
+                            </div>
                         </div>
                         <div className="ST_content_two_mobile">
-
+                            <div className="ST_Header_content_mobile">
+                                <span className="ST_Numbers_mobile">Numbers of <br/>Settlement</span>
+                                <span className="ST_Numbers_one_mobile">0</span>
+                            </div>
                         </div>
                         <div className="ST_content_three_mobile"> 
+                                <div  className="ST_Header_content_two_mobile">
+                                    <span className="ST_Total_mobile">Total<br/>Commissions</span>
+                                    <span className="ST_Total_one_mobile">N&nbsp;0.00</span>
+                                </div>
 
                         </div>
                         <div className="ST_content_four_mobile">
-
+                            <div style={{ height: 320, width: '100%' }}>
+                               <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
+                            </div>
                         </div>
                         
                     </div>
